9f0e9cba187372f490f4331be8ca8d9c04256855
picklerick94 <jastangting@gmail.com>
2022-10-30 11:30:38 -0700

    finished create and read api

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,8 +1,10 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 const USERNAME = encodeURIComponent("dbp2");
 const PASSWORD = encodeURIComponent("GBKMbJoZDhHcIwO0");
 const MONGO_URI= `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.6ud8xet.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(MONGO_URI);
+const DB_NAME = "sample_students";
+const COLLECTION_NAME = "studentslists";
 
 // create and save new students
 exports.create = async (req,res) => {
@@ -12,31 +14,72 @@ exports.create = async (req,res) => {
     return;
   }
 
-  // new student
-  const student = {
-    name: "Ting",
-    NUID: 1234567,
-    twitterAccount: "@jastangting",
-    tweets: [
-      {
-        content: "React fiber: In React V15, drop frame happens :( React V16 changed the stack reconciler to fiber reconciler, which is a singly-linked list with fiber nodes, to record prev and next steps. The traverse therefore can be stopped and restarted.",
-        timeStamp: Date.now(),
-        metDeadline: true
-      }
-    ]
-  }
-
   try {
-    const result = await client.db("sample_students").collection("studentslists").insertOne(student);
+    const result = await client.db(DB_NAME).collection(COLLECTION_NAME).insertOne(req.body);
     console.log(
-      `A document was inserted with the _id: ${result.insertedId}`,
+      `A student was inserted with the _id: ${result.insertedId}`,
     );
+    res.send(200);
   } catch(e) {
     console.log(e.message || "err ocurred while creating student");
   }
 }
 
 // find and return all students
-exports.find = (req,res) => {
+exports.find = async (req,res) => {
+    // validate request
+    if (!req.body) {
+      res.status(400).send({message: "student can not be empty"});
+      return;
+    }
 
+    try {
+      const result = await client.db(DB_NAME).collection(COLLECTION_NAME).find({}) .toArray();
+      console.log(
+        `All students: ${JSON.stringify(result)}`,
+      );
+      res.json(result);
+    } catch(e) {
+      console.log(e.message || "err ocurred while creating student");
+    }
+}
+
+// Update a student information
+exports.update = async (req,res) => {
+  // validate request
+  if (!req.body) {
+    res.status(400).send({message: "student can not be empty"});
+    return;
+  }
+
+  try {
+    const result = await client.db(DB_NAME).collection(COLLECTION_NAME).find({ name: "Ting" }).toArray();
+    console.log(
+      `All students: ${result}`,
+    );
+  } catch(e) {
+    console.log(e.message || "err ocurred while creating student");
+  } finally {
+    await client.close();
+  }
+}
+
+// Delete a student
+exports.delete = async (req,res) => {
+  // validate request
+  if (!req.body) {
+    res.status(400).send({message: "student can not be empty"});
+    return;
+  }
+
+  try {
+    const result = await client.db(DB_NAME).collection(COLLECTION_NAME).find({ name: "Ting" });
+    console.log(
+      `All students: ${result.name}`,
+    );
+  } catch(e) {
+    console.log(e.message || "err ocurred while creating student");
+  } finally {
+    await client.close();
+  }
 }
--- server/routes/router.js
+++ server/routes/router.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const route = express.Router();
 const controller = require('../controller/controller');
+let bodyParser = require('body-parser');
+
+route.use(bodyParser.json());
+// const myDB = require('../MyMongoDB/MyMongoDB');
 
 route.get('/', (req,res) =>{
   res.sendFile(path.resolve(path.join(process.cwd(), '/views/index.html')));

86de7d4223422b17c33d3f0c6b8449a3964de46e
DylanTse520 <31404393+DylanTse520@users.noreply.github.com>
2022-11-01 09:22:56 -0700

    Revert "refac"

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -1,104 +1,99 @@
-const Detail = () => {
-  const params = new Proxy(new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop),
-  });
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
+});
 
-  const studentId = params.id;
+const studentId = params.id;
 
-  const fetchAllTweets = async () => {
-    const res = await fetch("/api/students/" + studentId);
-    return await res.json();
-  };
+const fetchAllTweets = async () => {
+  const res = await fetch("/api/students/" + studentId);
+  return await res.json();
+};
 
-  const displayContent = async () => {
-    const infoSection = document.querySelector(".info-section");
-    infoSection.style.display = "none";
-    const tweetSection = document.querySelector(".tweet-section");
-    tweetSection.style.display = "none";
+const displayContent = async () => {
+  const infoSection = document.querySelector(".info-section");
+  infoSection.style.display = "none";
+  const tweetSection = document.querySelector(".tweet-section");
+  tweetSection.style.display = "none";
 
-    const container = document.querySelector(".container");
+  const container = document.querySelector(".container");
 
-    const loadingNotice = document.createElement("div");
-    loadingNotice.className = "notice";
-    loadingNotice.innerHTML = `
+  const loadingNotice = document.createElement("div");
+  loadingNotice.className = "notice";
+  loadingNotice.innerHTML = `
     Loading...
   `;
 
-    container.appendChild(loadingNotice);
+  container.appendChild(loadingNotice);
 
-    const allTweets = (await fetchAllTweets())[0];
+  const allTweets = (await fetchAllTweets())[0];
 
-    container.removeChild(loadingNotice);
-    infoSection.style.display = "flex";
-    tweetSection.style.display = "flex";
+  container.removeChild(loadingNotice);
+  infoSection.style.display = "flex";
+  tweetSection.style.display = "flex";
 
-    const totalCount = document.querySelector("#total-tweets-count");
-    totalCount.innerHTML = allTweets.tweets.length;
+  const totalCount = document.querySelector("#total-tweets-count");
+  totalCount.innerHTML = allTweets.tweets.length;
 
-    const studentInfo = document.querySelector("#student-info");
-    studentInfo.innerHTML = allTweets.name + " NUID " + allTweets.NUID;
+  const studentInfo = document.querySelector("#student-info");
+  studentInfo.innerHTML = allTweets.name + " NUID " + allTweets.NUID;
 
-    const twitterHandle = document.querySelector("#twitter-handle");
-    twitterHandle.innerHTML = "@" + allTweets.twitterAccount;
+  const twitterHandle = document.querySelector("#twitter-handle");
+  twitterHandle.innerHTML = "@" + allTweets.twitterAccount;
 
-    const content = document.querySelector(".tweets");
+  const content = document.querySelector(".tweets");
 
-    if (allTweets.tweets.length === 0) {
-      const emptyNotice = document.createElement("div");
-      emptyNotice.className = "empty-notice";
-      emptyNotice.innerHTML = `
+  if (allTweets.tweets.length === 0) {
+    const emptyNotice = document.createElement("div");
+    emptyNotice.className = "empty-notice";
+    emptyNotice.innerHTML = `
       No available tweets.
     `;
 
-      content.appendChild(emptyNotice);
-    }
+    content.appendChild(emptyNotice);
+  }
 
-    let missedTweetCount = 0;
+  let missedTweetCount = 0;
 
-    const startDate = new Date("September 13, 2022 13:00:00");
-    const endDate = new Date();
+  const startDate = new Date("September 13, 2022 13:00:00");
+  const endDate = new Date();
 
-    for (
-      let date = startDate;
-      date < endDate;
-      date.setDate(date.getDate() + 7)
-    ) {
-      let dueDate = new Date(date);
-      dueDate.setDate(date.getDate() + 7);
+  for (let date = startDate; date < endDate; date.setDate(date.getDate() + 7)) {
+    let dueDate = new Date(date);
+    dueDate.setDate(date.getDate() + 7);
 
-      const tweet = allTweets.tweets.find((tweet) => {
-        const dateTime = new Date(tweet.created_at);
-        return dateTime >= date && dateTime < dueDate;
-      });
+    const tweet = allTweets.tweets.find((tweet) => {
+      const dateTime = new Date(tweet.created_at);
+      return dateTime >= date && dateTime < dueDate;
+    });
 
-      if (tweet) {
-        const oneTweet = document.createElement("div");
-        const dateTime = new Date(tweet.created_at);
-        const dateFormatted =
-          dateTime.toLocaleString("default", {
-            month: "short",
-            day: "numeric",
-          }) +
-          " " +
-          dateTime.toLocaleString("default", {
-            timeStyle: "medium",
-          });
-        const dueDateFormatted =
-          dueDate.toLocaleString("default", {
-            month: "short",
-            day: "numeric",
-          }) +
-          " " +
-          dueDate.toLocaleString("default", {
-            timeStyle: "medium",
-          });
+    if (tweet) {
+      const oneTweet = document.createElement("div");
+      const dateTime = new Date(tweet.created_at);
 
-        oneTweet.className = "one-tweet";
-        oneTweet.innerHTML = `
-      <span class="date-time">${dateFormatted}</span>
+      oneTweet.className = "one-tweet";
+      oneTweet.innerHTML = `
+      <span class="date-time">${
+        dateTime.toLocaleString("default", {
+          month: "short",
+          day: "numeric",
+        }) +
+        " " +
+        dateTime.toLocaleString("default", {
+          timeStyle: "medium",
+        })
+      }</span>
       <span class="deadline-date-time">
         Post deadline:
-        <span>${dueDateFormatted}</span>
+        <span>${
+          dueDate.toLocaleString("default", {
+            month: "short",
+            day: "numeric",
+          }) +
+          " " +
+          dueDate.toLocaleString("default", {
+            timeStyle: "medium",
+          })
+        }</span>
       </span>
       <span class="content-title">Content</span>
       <span class="content">
@@ -106,17 +101,14 @@ const Detail = () => {
       </span>
     `;
 
-        content.appendChild(oneTweet);
-      } else {
-        missedTweetCount++;
-      }
+      content.appendChild(oneTweet);
+    } else {
+      missedTweetCount++;
     }
+  }
 
-    const missedCount = document.querySelector("#missed-tweets-count");
-    missedCount.innerHTML = missedTweetCount;
-  };
-
-  displayContent();
+  const missedCount = document.querySelector("#missed-tweets-count");
+  missedCount.innerHTML = missedTweetCount;
 };
 
-Detail();
+displayContent();
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,111 +1,107 @@
-const Index = () => {
-  const createNewStudent = async ({ name, nuid, twitterAccount }) => {
-    return await fetch("/api/student", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name,
-        NUID: nuid,
-        twitterAccount,
-      }),
-    });
-  };
-
-  const fetchAllStudents = async () => {
-    const res = await fetch("/api/students");
-    return await res.json();
-  };
-
-  const editStudent = async ({ id, name, nuid }) => {
-    return await fetch("/api/students/" + id, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name,
-        NUID: nuid,
-      }),
-    });
-  };
-
-  const deleteStudent = async (id) => {
-    return await fetch("/api/students/" + id, {
-      method: "DELETE",
-    });
-  };
-
-  const editModal = document.querySelector(".edit-modal");
-
-  editModal.addEventListener("click", (event) => {
-    if (event.target == editModal) {
-      editModal.style.display = "none";
-    }
+const createNewStudent = async ({ name, nuid, twitterAccount }) => {
+  return await fetch("/api/student", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      NUID: nuid,
+      twitterAccount,
+    }),
   });
+};
 
-  const editStudentForm = document.querySelector(".edit-student-form");
+const fetchAllStudents = async () => {
+  const res = await fetch("/api/students");
+  return await res.json();
+};
 
-  const displayContent = async () => {
-    const content = document.querySelector(".content");
+const editStudent = async ({ id, name, nuid }) => {
+  return await fetch("/api/students/" + id, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      NUID: nuid,
+    }),
+  });
+};
 
-    const loadingNotice = document.createElement("div");
-    loadingNotice.className = "notice";
-    loadingNotice.innerHTML = `
+const deleteStudent = async (id) => {
+  return await fetch("/api/students/" + id, {
+    method: "DELETE",
+  });
+};
+
+const editModal = document.querySelector(".edit-modal");
+
+editModal.addEventListener("click", (event) => {
+  if (event.target == editModal) {
+    editModal.style.display = "none";
+  }
+});
+
+const editStudentForm = document.querySelector(".edit-student-form");
+
+const displayContent = async () => {
+  const content = document.querySelector(".content");
+
+  const loadingNotice = document.createElement("div");
+  loadingNotice.className = "notice";
+  loadingNotice.innerHTML = `
     Loading...
   `;
 
-    content.appendChild(loadingNotice);
+  content.appendChild(loadingNotice);
 
-    const students = await fetchAllStudents();
+  const students = await fetchAllStudents();
 
-    const tweets = students.map((student) => {
-      const allTweets = student.tweets;
-      const mostRecentTweet = allTweets.reduce((mostRecent, tweet) =>
-        mostRecent.created_at > tweet.created_at ? mostRecent : tweet
-      );
-      const dateTime = new Date(mostRecentTweet.created_at);
-      return {
-        name: student.name,
-        NUID: student.NUID,
-        twitterAccount: "@" + student.twitterAccount,
-        dateTime:
-          dateTime.toLocaleString("default", {
-            month: "short",
-            day: "numeric",
-          }) +
-          " " +
-          dateTime.toLocaleString("default", {
-            timeStyle: "medium",
-          }),
-        content: mostRecentTweet.text,
-        studentId: student._id,
-      };
-    });
+  const tweets = students.map((student) => {
+    const allTweets = student.tweets;
+    const mostRecentTweet = allTweets.reduce((mostRecent, tweet) =>
+      mostRecent.created_at > tweet.created_at ? mostRecent : tweet
+    );
+    const dateTime = new Date(mostRecentTweet.created_at);
+    return {
+      name: student.name,
+      NUID: student.NUID,
+      twitterAccount: "@" + student.twitterAccount,
+      dateTime:
+        dateTime.toLocaleString("default", {
+          month: "short",
+          day: "numeric",
+        }) +
+        " " +
+        dateTime.toLocaleString("default", {
+          timeStyle: "medium",
+        }),
+      content: mostRecentTweet.text,
+      studentId: student._id,
+    };
+  });
 
-    while (content.firstChild) {
-      content.removeChild(content.firstChild);
-    }
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
 
-    if (tweets.length === 0) {
-      const emptyNotice = document.createElement("div");
-      emptyNotice.className = "notice";
-      emptyNotice.innerHTML = `
+  if (tweets.length === 0) {
+    const emptyNotice = document.createElement("div");
+    emptyNotice.className = "notice";
+    emptyNotice.innerHTML = `
       No available tweets.
     `;
 
-      content.appendChild(emptyNotice);
-    }
+    content.appendChild(emptyNotice);
+  }
 
-    tweets.forEach((tweet) => {
-      const contentCard = document.createElement("div");
-      const id = tweet.studentId;
-      const name = encodeURIComponent(tweet.name);
-      const nuid = tweet.NUID;
+  tweets.forEach((tweet) => {
+    const contentCard = document.createElement("div");
 
-      contentCard.className = "content-card";
-      contentCard.innerHTML = `
+    contentCard.className = "content-card";
+    contentCard.innerHTML = `
       <div class="personal-info">
         <img src="img/avatar-tweet.png" alt="The profile image for the twitter user.">
         <div class="info-list">
@@ -121,88 +117,87 @@ const Index = () => {
       <div class="date-time">${tweet.dateTime}</div>
       <div class="tweet-content">${tweet.content}</div>  
       <div class="operation">
-        <a class="detail-link" href="./student/${id}">Detail</a>
-        <button class="operation-button
-        edit-button"
-        data-studentid=${id} data-name=${name} data-nuid=${nuid}>
+        <a class="detail-link" href="./student/${tweet.studentId}">Detail</a>
+        <button class="operation-button edit-button" data-studentid=${
+          tweet.studentId
+        } data-name=${encodeURIComponent(tweet.name)} data-nuid=${tweet.NUID}>
           Edit
         </button>
-        <button class="operation-button delete-button" data-studentid=${id}>Delete</button>
+        <button class="operation-button delete-button" data-studentid=${
+          tweet.studentId
+        }>Delete</button>
       </div>
     `;
 
-      content.appendChild(contentCard);
-    });
-
-    const deleteButtons = document.querySelectorAll(".delete-button");
-    deleteButtons.forEach((button) => {
-      button.addEventListener("click", async (e) => {
-        const studentId = e.target.getAttribute("data-studentid");
-        await deleteStudent(studentId);
-        await displayContent();
-      });
-    });
-
-    const editButtons = document.querySelectorAll(".edit-button");
-    editButtons.forEach((button) => {
-      button.addEventListener("click", (e) => {
-        editModal.style.display = "block";
-        const id = e.target.getAttribute("data-studentid");
-        const name = decodeURIComponent(e.target.getAttribute("data-name"));
-        const nuid = e.target.getAttribute("data-nuid");
-        editStudentForm.elements["name"].value = name;
-        editStudentForm.elements["nuid"].value = nuid;
-        editStudentForm.elements["id"].value = id;
-      });
-    });
-  };
-
-  displayContent();
-
-  const createButton = document.querySelector("#create-button");
-  const createModal = document.querySelector(".create-modal");
-
-  createButton.addEventListener("click", () => {
-    createModal.style.display = "block";
+    content.appendChild(contentCard);
   });
 
-  createModal.addEventListener("click", (event) => {
-    if (event.target == createModal) {
-      createModal.style.display = "none";
-    }
-  });
-
-  const cancelButtons = document.querySelectorAll(".cancel-button");
-
-  cancelButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      createModal.style.display = "none";
-      editModal.style.display = "none";
+  const deleteButtons = document.querySelectorAll(".delete-button");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", async (e) => {
+      const studentId = e.target.getAttribute("data-studentid");
+      await deleteStudent(studentId);
+      await displayContent();
     });
   });
 
-  const createStudentForm = document.querySelector(".create-student-form");
-  createStudentForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const formProps = Object.fromEntries(formData);
-
-    await createNewStudent(formProps);
-    createStudentForm.reset();
-    createModal.style.display = "none";
-    await displayContent();
-  });
-
-  editStudentForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const formProps = Object.fromEntries(formData);
-
-    await editStudent(formProps);
-    editStudentForm.reset();
-    editModal.style.display = "none";
-    await displayContent();
+  const editButtons = document.querySelectorAll(".edit-button");
+  editButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      editModal.style.display = "block";
+      const id = e.target.getAttribute("data-studentid");
+      const name = decodeURIComponent(e.target.getAttribute("data-name"));
+      const nuid = e.target.getAttribute("data-nuid");
+      editStudentForm.elements["name"].value = name;
+      editStudentForm.elements["nuid"].value = nuid;
+      editStudentForm.elements["id"].value = id;
+    });
   });
 };
 
-Index();
+displayContent();
+
+const createButton = document.querySelector("#create-button");
+const createModal = document.querySelector(".create-modal");
+
+createButton.addEventListener("click", () => {
+  createModal.style.display = "block";
+});
+
+createModal.addEventListener("click", (event) => {
+  if (event.target == createModal) {
+    createModal.style.display = "none";
+  }
+});
+
+const cancelButtons = document.querySelectorAll(".cancel-button");
+
+cancelButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    createModal.style.display = "none";
+    editModal.style.display = "none";
+  });
+});
+
+const createStudentForm = document.querySelector(".create-student-form");
+createStudentForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const formProps = Object.fromEntries(formData);
+
+  await createNewStudent(formProps);
+  createStudentForm.reset();
+  createModal.style.display = "none";
+  await displayContent();
+});
+
+editStudentForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const formProps = Object.fromEntries(formData);
+
+  await editStudent(formProps);
+  editStudentForm.reset();
+  editModal.style.display = "none";
+  await displayContent();
+});
